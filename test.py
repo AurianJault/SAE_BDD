d915a589a2673adb1f41f06f16e8e30453ab1653
Remi ARNAL <Remi.ARNAL@etu.uca.fr>
2022-05-18 09:20:57 +0200

    add to test + rename md

--- test.py
+++ test.py
@@ -3,9 +3,10 @@ import pandas as pd
 data = pd.read_csv('src/amazon.csv', low_memory=False)
 df = pd.DataFrame(data)
 
+#cut the £ sign in price column
 var = df['price'].str.split('£')
 print(var.str[1])
 
-print(df['uniq_id'])
-
-df.head()
+#cut the number_available column to have number and new/used
+var1 = df['average_review_rating'].str.split(' ')
+print(var1.str[0])
